6b422d235c77170f62fafc6c4205795f9ad2738c
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2023-09-20 20:57:53 -0600

    creacion de la clase DisposicionMuelle, para hacer uso de la Disposicion de muelle

---
+++ src/graficos/DisposicionMuelle.java
@@ -0,0 +1,62 @@
+package graficos;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class DisposicionMuelle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MarcoMuelle mimarco=new MarcoMuelle();
+		
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+
+class MarcoMuelle extends JFrame{
+	
+	public MarcoMuelle() {
+		
+		setBounds(300,400,1000,350);
+		
+		laminaMuelle milamina=new laminaMuelle();
+		
+		add(milamina);
+		
+		setVisible(true);
+	}
+	
+}
+
+class laminaMuelle extends JPanel{
+	
+	public laminaMuelle() {
+		
+		JButton boton1=new JButton("boton 1");
+		
+		JButton boton2=new JButton("boton 2");
+		
+		JButton boton3=new JButton("boton 3");
+		
+		SpringLayout milayout=new SpringLayout();
+		
+		setLayout(milayout);
+		
+		add(boton1);
+		
+		add(boton2);
+		
+		add(boton3);
+		
+		Spring mimuelle=Spring.constant(0, 10, 100);
+		
+		milayout.putConstraint(SpringLayout.WEST, boton1, mimuelle, SpringLayout.WEST, this);
+		
+		milayout.putConstraint(SpringLayout.WEST, boton2, mimuelle, SpringLayout.EAST, boton1);
+		
+		milayout.putConstraint(SpringLayout.WEST, boton3, mimuelle, SpringLayout.EAST, boton2);
+		
+		milayout.putConstraint(SpringLayout.EAST, this, mimuelle, SpringLayout.EAST, boton3);
+		
+	}
+}
