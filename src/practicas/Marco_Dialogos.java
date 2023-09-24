54c6a3e1a05957aa10f2cdfe6dcfbef619285980
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2023-09-24 14:37:44 -0600

    Creacion de la interfaz Prueba dialogos, vista

--- src/practicas/Marco_Dialogos.java
+++ src/practicas/Marco_Dialogos.java
@@ -20,11 +20,71 @@ public class Marco_Dialogos extends JFrame {
 
 		lamina_tipo=new Lamina_Botones("Tipo", primero);
 		
-		lamina_cuadricula.add(lamina_tipo);
+		lamina_tipo_mensajes=new Lamina_Botones("Tipo de mensaje", new String[] {
+				
+				"ERROR_MESSAGE",
+				"INFORMATION_MENSSAGE",
+				"QUESTION_MESSAGE",
+				"PLAIN_MESSEGE"
 		
-		add(lamina_cuadricula);
+		});
+		
+		lamina_mensaje=new Lamina_Botones("Mensaje", new String[] {
+				
+				"Cadena",
+				"Icono",
+				"Componente",
+				"Otros",
+				"Object[]"
+		
+		});
+		
+		lamina_tipo_opcion=new Lamina_Botones("Confirmar", new String[] {
+				
+				"DEFAULT_OPTION",
+				"YES_NO_OPTION",
+				"YES_NO_CANCEL_OPTION",
+				"OK_CANCEL_OPTION"
+		
+		});
+		
+		lamina_opciones=new Lamina_Botones("Opción", new String[] {
+				
+				"String[ ]",
+				"Icon[ ]",
+				"Object[ ]"
+		
+		});
+		
+		lamina_entrada=new Lamina_Botones("Entrada", new String[] {
+				
+				"Campo de texto",
+				"Combo",
+		
+		});
+		
+		setLayout(new BorderLayout());
+		
+		lamina_cuadricula.add(lamina_tipo);
+		lamina_cuadricula.add(lamina_tipo_mensajes);
+		lamina_cuadricula.add(lamina_mensaje);
+		lamina_cuadricula.add(lamina_tipo_opcion);
+		lamina_cuadricula.add(lamina_opciones);
+		lamina_cuadricula.add(lamina_entrada);
+		
+		// Construimos la lámina inferior
+		
+		JPanel lamina_mostrar=new JPanel();
+		
+		JButton boton_mostrar=new JButton("Mostrar");
+		
+		lamina_mostrar.add(boton_mostrar);
+		
+		add(lamina_mostrar, BorderLayout.SOUTH);
+		
+		add(lamina_cuadricula, BorderLayout.CENTER);
 		
 	}	
 	
-	private Lamina_Botones lamina_tipo;
+	private Lamina_Botones lamina_tipo, lamina_tipo_mensajes, lamina_mensaje, lamina_tipo_opcion, lamina_opciones, lamina_entrada;
 }
